02ea87822dbbe9bfa3d794e549547b5265f6b8dd
milleracero <mchicaa@unal.edu.co>
2022-09-25 02:43:33 -0500

    2

--- src/main/java/Taller5/src/gestion/Zoologico.java
+++ src/main/java/Taller5/src/gestion/Zoologico.java
@@ -1,5 +1,48 @@
 package gestion;
 
-public class Zoologico {
+import java.util.ArrayList;
 
+public class Zoologico {
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zonas;
+	
+	public Zoologico() {
+		
+	}
+	public Zoologico(String nombre, String ubicacion) {
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public ArrayList<Zona> getZonas() {
+		return zonas;
+	}
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+	
+	public void agregarZonas(Zona zonaNueva) {
+		zonas.add(zonaNueva);
+		zonaNueva.setZoo(this);
+	}
+	
+	public int cantidadTotalAnimales() {
+		int n=0;
+		for (int i=0;i<zonas.size();i++) {
+			n=n+zonas.get(i).cantidadAnimales();
+			}
+		return n;
+		}
 }
